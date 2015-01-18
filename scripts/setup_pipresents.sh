d99c9a4fe2169c264c2997f43ac32fb41d64b151
Cornelius Wichmann <c.wichmann@gmx.de>
2015-01-18 19:27:40 +0100

    change profile for liveshow

--- scripts/setup_pipresents.sh
+++ scripts/setup_pipresents.sh
@@ -7,22 +7,23 @@ sudo apt-get -y install x11-xserver-utils
 sudo apt-get -y install unclutter
 sudo apt-get -y install mplayer
 sudo apt-get -y install uzbl
+sudo apt-get -y install usbmount
 # Install pexpect
 cd ~
 wget http://pexpect.sourceforge.net/pexpect-2.3.tar.gz
 tar xzf pexpect-2.3.tar.gz
 cd pexpect-2.3
 sudo python ./setup.py install
-# install pipresents
-# sudo apt-get -y install gnome-keyring
+# Install pipresents
 cd ~
 rm -rf KenT2-pipresents*
 wget https://github.com/KenT2/pipresents-next/tarball/master -O - | tar xz
 mv KenT2-pipresents-next-* pipresents
+## Install Profiles
 cp -r ~/pipresents/pp_home ~/pp_home
 rm -rf ~/pp_home/pp_profiles/*
-mkdir /home/pi/pp_home/pp_profiles/livephoto
-cd ~/pp_home/pp_profiles/livephoto
+mkdir /home/pi/pp_home/pp_profiles/pp_usb
+cd ~/pp_home/pp_profiles/pp_usb
 wget https://raw.githubusercontent.com/reddipped/PIChannel/master/pichannel/gpio.cfg
 wget https://raw.githubusercontent.com/reddipped/PIChannel/master/pichannel/media.json
 wget https://raw.githubusercontent.com/reddipped/PIChannel/master/pichannel/pp_showlist.json
