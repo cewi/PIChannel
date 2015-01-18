7767c755248e538628aed45b619f9396ccf07aaa
Cornelius Wichmann <c.wichmann@gmx.de>
2015-01-18 21:10:21 +0100

    change setup
    change profile
    configure usmount

--- scripts/setup_pipresents.sh
+++ scripts/setup_pipresents.sh
@@ -8,6 +8,8 @@ sudo apt-get -y install unclutter
 sudo apt-get -y install mplayer
 sudo apt-get -y install uzbl
 sudo apt-get -y install usbmount
+## Configure usbmount
+sudo sed --in-place "s/^FS_MOUNTOPTIONS=\"\"/FS_MOUNTOPTIONS=\"\-fstype=vfat,gid=users,dmask=0007,fmask=0117\"/" /etc/usbmount/usbmount.conf
 # Install pexpect
 cd ~
 wget http://pexpect.sourceforge.net/pexpect-2.3.tar.gz
@@ -21,13 +23,14 @@ wget https://github.com/KenT2/pipresents-next/tarball/master -O - | tar xz
 mv KenT2-pipresents-next-* pipresents
 ## Install Profiles
 cp -r ~/pipresents/pp_home ~/pp_home
+mkdir /home/pi/pp_home/pp_live_tracks
 rm -rf ~/pp_home/pp_profiles/*
-mkdir /home/pi/pp_home/pp_profiles/pp_usb
-cd ~/pp_home/pp_profiles/pp_usb
-wget https://raw.githubusercontent.com/reddipped/PIChannel/master/pichannel/gpio.cfg
-wget https://raw.githubusercontent.com/reddipped/PIChannel/master/pichannel/media.json
-wget https://raw.githubusercontent.com/reddipped/PIChannel/master/pichannel/pp_showlist.json
-wget https://raw.githubusercontent.com/reddipped/PIChannel/master/pichannel/resources.cfg
+mkdir /home/pi/pp_home/pp_profiles/pp_live
+cd ~/pp_home/pp_profiles/pp_live
+wget https://raw.githubusercontent.com/cewi/PIUSB/master/pp_live/gpio.cfg
+wget https://raw.githubusercontent.com/cewi/PIUSB/master/pp_live/liveshow.json
+wget https://raw.githubusercontent.com/cewi/PIUSB/master/pp_live/pp_showlist.json
+wget https://raw.githubusercontent.com/cewi/PIUSB/master/pp_live/resources.cfg
 mkdir ~/pp_home/media
 # prepare auto start
 cd /var/tmp
